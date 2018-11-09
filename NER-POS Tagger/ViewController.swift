164753d2c39ac0ad8e48a991377cdfac7bcd06ae
adityajoshi1994 <adityarajendrajoshi@gmail.com>
2018-11-08 19:49:52 -0800

    Initial Commit

---
+++ NER-POS Tagger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NER-POS Tagger
+//
+//  Created by Aditya Joshi on 11/8/18.
+//  Copyright © 2018 Aditya Joshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
